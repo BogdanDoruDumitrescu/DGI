04d532ddcc34f10760ce969b51dccbf6fcaf5a29
Denis Gruia <denis.gruiax@outlook.com>
2020-06-29 20:10:30 +0300

    DGI-2 Implemented function for registration and "AlreadyExists" exception

--- src/main/java/org/fis/maven/Controllers/RegisterController.java
+++ src/main/java/org/fis/maven/Controllers/RegisterController.java
@@ -6,9 +6,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.fis.maven.Exceptions.AlreadyExists;
+import org.fis.maven.Models.Admin;
+import org.fis.maven.Models.User;
+import org.fis.maven.Services.AdminService;
+import org.fis.maven.Services.UserService;
 
 import java.io.IOException;
 
@@ -25,8 +31,17 @@ public class RegisterController {
     private ChoiceBox role;
     @FXML
     private TextField creditField;
-
     @FXML
+    private Label error;
+
+    public void initialize(){
+        role.getItems().addAll("Admin", "Driver", "Client");
+        role.setValue("Driver");
+        AdminService.loadAdmins();
+        UserService.loadUser();
+        error.setText("");
+    }
+
     public void backButton()
     {
         try
@@ -42,6 +57,33 @@ public class RegisterController {
     }
 
     public void signUp() {
-        //
+        try {
+            if (role.getValue().equals("Admin")) {
+                for (Admin i : AdminService.getA()) {
+                    if (i.getUsername().equals(usernameField.getText())) {
+                        throw new AlreadyExists();
+                    }
+                }
+
+                AdminService.getA().add(new Admin(nameField.getText(), usernameField.getText(), AdminService.encodePassword(passwordField.getText()), mailField.getText()));
+                AdminService.writeAdmins();
+            } else {
+                for(User i: UserService.getU()){
+                    if(i.getUsername().equals(usernameField.getText())){
+                        throw new AlreadyExists();
+                    }
+                }
+
+                User user = new User(nameField.getText(), usernameField.getText(), UserService.encodePassword(passwordField.getText()), mailField.getText(), role.getValue().toString(), Integer.parseInt(creditField.getText()));
+                UserService.getU().add(user);
+                if(role.getValue().equals("Driver")){
+                    user.setStatus("Available");
+                }
+                UserService.writeUser();
+                error.setText("Done!");
+            }
+        }catch(Exception e){
+            error.setText("Account already exists!");
+        }
     }
 }
