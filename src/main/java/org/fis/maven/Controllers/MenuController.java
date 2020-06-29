4329a1e7646daced9bbf591b2dc09df99c4a1ab7
Denis Gruia <denis.gruiax@outlook.com>
2020-06-29 19:38:01 +0300

    DGI-2 Menu window added

---
+++ src/main/java/org/fis/maven/Controllers/MenuController.java
@@ -0,0 +1,37 @@
+package org.fis.maven.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+
+import java.io.IOException;
+
+public class MenuController {
+
+    @FXML
+    private Label id;
+
+    @FXML
+    public void registerButton(){
+        //register
+    }
+
+    @FXML
+    public void loginAdministrator(){
+        //login admin
+    }
+
+    @FXML
+    public void loginClient(){
+        //login client
+    }
+
+    @FXML
+    public void loginDriver(){
+        //login driver
+    }
+}
--- src/main/java/org/fis/maven/Main.java
+++ src/main/java/org/fis/maven/Main.java
@@ -1,9 +1,14 @@
 package org.fis.maven;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Main {
+public class Main extends Application {
     public static void main(String[] args){
         Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
@@ -12,5 +17,15 @@ public class Main {
         LOGGER.info("This is an INFO log");
         LOGGER.warn("This is a WARN log");
         LOGGER.error("This is an ERROR log");
+
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Menu.fxml"));
+        primaryStage.setTitle("Menu");
+        primaryStage.setScene(new Scene(root,600,600));
+        primaryStage.show();
     }
 }
